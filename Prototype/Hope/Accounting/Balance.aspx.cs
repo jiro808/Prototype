064dda7c7df3c9e9d06892298a45811e2ffccb18
KertDelosSantos <delossantos.kert.12251999@gmail.com>
2023-11-29 14:23:13 +0800

    asd

--- Prototype/Hope/Accounting/Balance.aspx.cs
+++ Prototype/Hope/Accounting/Balance.aspx.cs
@@ -55,8 +55,6 @@ namespace Prototype.Hope.Accounting
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-
-                // Use parameterized query to avoid SQL injection
                 string query = "SELECT OB.invoice_no, S.student_id, S.name, OB.date, OB.total, OB.due, OB.status FROM OverdueBalance AS OB INNER JOIN Student AS S ON OB.student_id = S.student_id  WHERE  S.student_id LIKE @studentid";
 
                 using (SqlCommand command = new SqlCommand(query, connection))
@@ -66,15 +64,12 @@ namespace Prototype.Hope.Accounting
                     {
                         DataTable dt = new DataTable();
                         adapter.Fill(dt);
-
                         GridView2.DataSource = dt;
                         GridView2.DataBind();
-
-                        // Set the GridView in edit mode for the first row (adjust as needed)
                         if (dt.Rows.Count > 0)
                         {
                             GridView2.EditIndex = 0;
-                            GridView2.DataBind();  // Rebind to enter edit mode
+                            GridView2.DataBind();
                         }
                     }
                 }
@@ -89,25 +84,20 @@ namespace Prototype.Hope.Accounting
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    // If "All" is selected, show all statuses
                     if (rblFilter.SelectedValue == "All")
                     {
-                        command.Parameters.AddWithValue("@status", "%"); // Use '%' to match all statuses
+                        command.Parameters.AddWithValue("@status", "%");
                     }
                     else
                     {
                         command.Parameters.AddWithValue("@status", rblFilter.SelectedValue);
                     }
-
                     using (SqlDataAdapter adapter = new SqlDataAdapter(command))
                     {
-
                         DataTable dt = new DataTable();
                         adapter.Fill(dt);
-
-                            GridView2.DataSource = dt;
-                            GridView2.DataBind();
-
+                        GridView2.DataSource = dt;
+                        GridView2.DataBind();
                     }
                 }
             }
@@ -126,10 +116,7 @@ namespace Prototype.Hope.Accounting
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-
-                // Use parameterized query to avoid SQL injection
                 string query = "UPDATE OverdueBalance SET [status] = @status WHERE invoice_no = @invoice_no";
-
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@status", status);
@@ -143,9 +130,7 @@ namespace Prototype.Hope.Accounting
         {
             if (GridView2.EditIndex == e.NewEditIndex)
             {
-                // GridView is in edit mode, proceed with accessing the DropDownList
                 DropDownList DropDown = (DropDownList)GridView2.Rows[e.NewEditIndex].FindControl("DropDownList1");
-                // Rest of your code...
             }
             GridView2.EditIndex = e.NewEditIndex;
             binddata();
